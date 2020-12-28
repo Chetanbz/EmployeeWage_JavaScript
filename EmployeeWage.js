02b5447f10a4a48bbe39143d414efefd0e4e3638
chetan <zambare.c123@gmail.com>
2020-12-28 17:45:46 +0530

    [Chetan] Added: Employee Wage for Month Max Works Hrs Set

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,13 +5,19 @@ const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HOURS = 20;
 const maxWorkDay = 20;
+const maxWorkHrsMonth = 160;
+
 let empHrs = 0;
-let monthWorkHrs =0 ;
+var monthWorkHrs =0 ;
 let employeeType;
 
 for (var i = 1; i <= maxWorkDay; i++){
     employeeType = Math.floor(Math.random()*10)%3;
     monthWorkHrs += calculateEmpHrs(employeeType);
+    if(monthWorkHrs >=maxWorkHrsMonth){
+        monthWorkHrs = maxWorkHrsMonth;
+        break;
+    }
 }
 
 let totalMonthWage = monthWorkHrs*WAGE_PER_HOURS;
@@ -19,10 +25,10 @@ console.log("Total Employee Hrs " + monthWorkHrs+ " Employee Month Wgae is " + t
 
 function calculateEmpHrs(employeeType){
     if(employeeType == IS_PART_TIME){
-        empHrs +=4;
+        empHrs = 4;
     }
     else if (employeeType == IS_FULL_TIME){
-        empHrs +=8 ;
+        empHrs = 8 ;
     }
     return empHrs;
 }
