5d94e79ca66ee6336782ddaa3ae3cc8a7cfae5d9
chetan <zambare.c123@gmail.com>
2020-12-28 15:29:58 +0530

    [Chetan] Added: Employee Wage calculation using Function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,30 +1,25 @@
-const IS_ABSENT = 0;
-let empCheck = Math.floor(Math.random()*10)%2;
 
-if (empCheck == IS_ABSENT ){
-    console.log("Employee is Absent, Exit from program");
-    return;
-}else{
-    console.log("Employee is Present");
-}
-
-
-//UC2
-const IS_PART_TIME = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HOURS = 20;
 
 let empHrs = 0;
 let totalEmployeeWage = 0;
-let partTimeCheck = Math.floor(Math.random()*10)%2;
+let employeeType = Math.floor(Math.random()*10)%3;
 
-if(partTimeCheck == IS_PART_TIME){
-    empHrs +=4;
-}
-else{
-    empHrs +=8;
-}
 
-totalEmployeeWage = empHrs*WAGE_PER_HOURS;
+totalEmployeeWage = calculateEmpHrs(employeeType)*WAGE_PER_HOURS;
 console.log("Employee Wage is " + totalEmployeeWage );
+
+
+function calculateEmpHrs(employeeType){
+    if(employeeType == IS_PART_TIME){
+        empHrs +=4;
+    }
+    else if (employeeType == IS_FULL_TIME){
+        empHrs +=8 ;
+    }
+    return empHrs;
+}
